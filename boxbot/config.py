f35e847358b81b1f517344450aa8dc4ab312a94e
Andrei Dore <andrei.dore@gmail.com>
2023-07-22 19:59:50 +0300

    pylint  config.py

--- boxbot/config.py
+++ boxbot/config.py
@@ -1,3 +1,9 @@
+"""
+
+Configuration file for boxbot
+
+"""
+
 __VERSION__ = "0.1.0"
 
 ZMQ_HOST = "127.0.0.1"
@@ -14,13 +20,13 @@ VISION_ZMQ_PORT = "5004"
 NAVIGATION_ZMQ_PORT = "5002"
 
 # vision
-VISION_ENDPOINT = "tcp://{}:5004".format(ZMQ_HOST)
+VISION_ENDPOINT = f"tcp://{ZMQ_HOST}:5004"
 
-VISION_IMAGE_TOPIC = "tcp://{}:5001".format(ZMQ_HOST)
+VISION_IMAGE_TOPIC = f"tcp://{ZMQ_HOST}:5001"
 
 # Board
-BOARD_MOTOR_VELOCITY_SERVICE = "tcp://{}:5010".format(ZMQ_HOST)
+BOARD_MOTOR_VELOCITY_SERVICE = f"tcp://{ZMQ_HOST}:5010"
 
-## camera
+# camera
 
 CAMERA_URI = "v4l2:///dev/video0"
